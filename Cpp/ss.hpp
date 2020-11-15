eeb7196b01062dcfc12f4bb7fb2a8678f4902ae7
extratorsion <extratorsion@outlook.com>
2020-11-15 23:04:53 +0800

    Add signal_slot simple impl.

--- Cpp/ss.hpp
+++ Cpp/ss.hpp
@@ -6,7 +6,7 @@
 
 struct SS {
   using SlotSet = HashMap<void*, std::list<pair<void*, Fn<void*()>>>>;
-  static SlotSet slots;
+  static SlotSet slots_;
 
   template <typename Class, typename T, typename... Args>
   fn connect(Class* obj, void (Class::*p)(Args...), T&& val)->void {
@@ -14,18 +14,18 @@ struct SS {
     Fn<void*()> fun = [_fun = raw_fun]() mutable {
       return reinterpret_cast<void*>(&_fun);
     };
-    if (let iter = slots.find(static_cast<void*>(obj)); iter != slots.end()) {
+    if (let iter = slots_.find(static_cast<void*>(obj)); iter != slots_.end()) {
       iter->second.push_back({pointer_cast(p), std::move(fun)});
     } else {
       std::list<pair<void*, Fn<void*()>>> l;
       l.push_back({pointer_cast(p), std::move(fun)});
-      slots.insert({static_cast<void*>(obj), std::move(l)});
+      slots_.insert({static_cast<void*>(obj), std::move(l)});
     }
   }
 
   template <typename Class, typename... Args>
   fn emit(Class* obj, void (Class::*p)(Args...), Args... args)->void {
-    if (let iter = slots.find(static_cast<void*>(obj)); iter != slots.end()) {
+    if (let iter = slots_.find(static_cast<void*>(obj)); iter != slots_.end()) {
       for (auto& [fun_sig, p_fun] : iter->second) {
         if (pointer_cast(p) == fun_sig) {
           auto fun = reinterpret_cast<Fn<void(Args...)>*>(p_fun());
@@ -35,19 +35,20 @@ struct SS {
     }
   }
 
+  ~SS() {
+    if (auto iter = slots_.find(static_cast<void*>(this)); iter != slots_.end()) {
+      slots_.erase(iter);
+    }
+  }
+
+private:
   template <typename Class, typename... Args>
   fn pointer_cast(void (Class::*p)(Args...))->void* {
     return (void*)(*reinterpret_cast<size_t*>(&p));
   }
-
-  ~SS() {
-    if (auto iter = slots.find(static_cast<void*>(this)); iter != slots.end()) {
-      slots.erase(iter);
-    }
-  }
 };
 
 
-SS::SlotSet SS::slots;
+SS::SlotSet SS::slots_;
 
 #endif
